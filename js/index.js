5a9eed9e3696b73b30ef8365ebb4a0814d201bcf
Raj poudel <92821156+rajpoudell@users.noreply.github.com>
2023-06-15 21:26:00 +0545

    Update index.js
    commenting variable what it can be used for

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 
 let inputDir = {x: 0, y: 0};
-const foodSound = new Audio('music/food.mp3');
+const foodSound = new Audio('music/food.mp3'); 
 const gameOverSound = new Audio('music/gameover.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
-let speed = 5;
-let score = 0;
+let speed = 5; //speed
+let score = 0; // initial score 
 lastPaintTime = 0;
-let snakeArr = [
+let snakeArr = [ // object inside array
     {x: 13, y:15}
 ];
 
